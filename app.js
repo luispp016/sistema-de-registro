fdf281a86d1cae25746469dd329872ff871333f4
Luis Pinto <luispp97@gmail.com>
2025-03-17 19:10:21 -0500

    Actualizacion de archivos base

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
+<<<<<<< HEAD
 console.log("Ejecutando el sistema")
+=======
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+>>>>>>> fcf822f (Actualizacion de archivos base)
